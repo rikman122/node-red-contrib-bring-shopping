4ac4af92d01f00a2bbd75ecec59570b8d7d90543
mdhom <max@dhom.info>
2020-10-05 11:35:58 +0200

    Initial commit

---
+++ install.ps1
@@ -0,0 +1,116 @@
+function copy-files {
+    PARAM (
+        $src,
+        $destRoot,
+        $destFolder
+    ) 
+
+    $src = Resolve-Path $src
+    $destRoot = Resolve-Path $destRoot
+
+    Write-Output "From : $src"
+    Write-Output "To   : $destRoot"
+
+    $files = Get-ChildItem -Recurse $src | Where-Object { ! $_.PSIsContainer }
+    foreach ($file in $files) {
+        $relativeFile = $file.FullName.Substring($src.Path.Length)
+        $dest = Join-Path -Path $destRoot -ChildPath "$destFolder\$relativeFile"
+
+        $folder = $dest.Substring(0, $dest.LastIndexOf("\"))
+        if (-not (Test-Path $folder)) {
+            New-Item -ItemType Directory $folder
+        }
+
+        Write-Output $dest
+        [System.IO.File]::Copy($file.FullName, $dest, $true);
+    }
+}
+
+if (-not (Test-Path ".\debug\package.json")) {
+    $check = docker container inspect -f '{{.State.Running}}' "nodered_testing"
+
+    if ($check) {
+        docker-compose down  
+    }
+
+    docker-compose up -d   
+
+    Write-Host "Waiting for node-red to boot ..." -NoNewLine 
+    while (!(Test-Path ".\debug\package.json")) { 
+        Start-Sleep 2
+    }
+    
+    Write-Host -ForegroundColor Green " done"
+}
+
+docker-compose down
+
+$origin = [Environment]::CurrentDirectory;
+$repoName = $origin.Split('\') | Select-Object -Last 1
+Write-Host "Name: $repoName"
+if (-not (Test-Path ".\debug\myModules\$repoName")) {
+    Write-Output "Creating Path..."
+    New-Item -ItemType Directory ".\debug\myModules\$repoName"
+    New-Item -ItemType Directory ".\debug\myModules\$repoName\lib"
+}
+
+Write-Output "Copy files..."
+
+copy-files -src '.\src' -destRoot ".\debug\myModules\$repoName" -destFolder "src"
+copy-files -src '.\lib' -destRoot ".\debug\myModules\$repoName" -destFolder "lib"
+
+$packageModified = $false 
+$destRoot = Resolve-Path ".\debug\myModules\$repoName"
+# Check if package.json needs install
+if (Test-Path $destRoot/package.json) {
+    Write-Output "Comparing package.json ..."
+    $packageModified = (Get-FileHash ./package.json).hash -ne (Get-FileHash $destRoot/package.json).hash
+    if ($packageModified) {
+        Write-Host "package.json was modified. Reinstall again after copy!" -ForegroundColor Red
+    }
+    else {
+        Write-Host "Unmodified. No reinstallation needed!" -ForegroundColor Green
+    }
+}
+else {
+    $packageModified = $true;
+}
+
+Copy-Item -Path ./package.json -Destination $destRoot/package.json
+
+
+$destination = Resolve-Path .\debug\
+Write-Output "Change location to: $destination"
+Set-Location $destination
+
+if ($packageModified) {
+    npm install ".\myModules\$repoName"
+}
+
+Write-Output "Switching back to: $origin"
+Set-Location $origin
+
+# fix for windwos docker
+# windows docker can't handle links so we need to copy the files....
+if ($IsWindows -or $Env:OS -eq "Windows_NT") {
+    Write-Host "Fixing Windows Docker Problems" -ForegroundColor Red
+    Write-Host "Resolve symlink" -ForegroundColor Yellow
+    $files = Get-ChildItem "./debug/node_modules/" | Where-Object { $_.Attributes -match "ReparsePoint" }
+    
+    Write-Host "Updating modules..." -ForegroundColor Yellow
+    $files = Get-ChildItem "./debug/node_modules/" -Directory
+    foreach ($file in $files) {
+        
+        $moduleName = $file.FullName.Split("\") | Select-Object -Last 1
+
+        if (Test-Path "./debug/myModules/$moduleName") {
+            Write-Host "Update module: " -NoNewline -ForegroundColor Magenta
+            Write-Host $moduleName
+            Remove-Item $file.FullName -Force -Recurse
+            Copy-Item -Recurse -Path "./debug/myModules/$moduleName/" -Destination "./debug/node_modules/"
+        }
+
+    }
+}
+
+docker-compose up -d
---
+++ src/bring.js
@@ -0,0 +1,304 @@
+const bringApi = require('bring-shopping');
+
+module.exports = function(RED) {
+    function bringCredentials(config) {
+        RED.nodes.createNode(this,config);
+        this.name   = config.name;
+
+        this.getBring = async function(){
+            if (this.bring === undefined) {
+                this.bring = new bringApi({ mail: this.credentials.email, password: this.credentials.password});
+                await this.bring.login();
+            }
+
+            return this.bring;
+        }
+
+        this.loadLists = async function() {
+            const bring = await this.getBring();
+            return await bring.loadLists();
+        }
+
+        this.getItems = async function (listUUID) {
+            const bring = await this.getBring();
+            return await bring.getItems(listUUID);
+        }
+
+        this.loadTranslations = async function (locale) {
+            const bring = await this.getBring();
+            return await bring.loadTranslations(locale);
+        }
+
+        this.loadCatalog = async function (locale) {
+            const bring = await this.getBring();
+            return await bring.loadCatalog(locale);
+        }
+
+        this.getItemsDetails = async function(listUUID) {
+            const bring = await this.getBring();
+            return await bring.getItemsDetails(listUUID);
+        }
+
+        this.removeItem = async function(listUUID, itemName) {
+            const bring = await this.getBring();
+            return await bring.removeItem(listUUID, itemName);
+        }
+
+        this.moveToRecently = async function(listUUID, itemName) {
+            const bring = await this.getBring();
+            return await bring.moveToRecentList(listUUID, itemName);
+        }
+
+        this.saveItem = async function(listUUID, itemName, specification) {
+            const bring = await this.getBring();
+            return await bring.saveItem(listUUID, itemName, specification);
+        }
+    }
+    RED.nodes.registerType('bringCredentials',bringCredentials, {
+		credentials: {
+			email:           { type:"text", required: true },
+            password:        { type:"text", required: true }
+		}
+    });
+
+
+    function loadListsNode(config) {
+        RED.nodes.createNode(this,config);
+        var bringCredentials = RED.nodes.getNode(config.bringCredentials);
+        var node = this;
+
+        node.on('input', async (msg) => {
+            try {          
+                msg.lists = await bringCredentials.loadLists();
+                node.send(msg);
+            } catch (err) {
+                node.error(err);
+            }
+        });
+    }
+    RED.nodes.registerType('loadLists',loadListsNode);
+
+
+    function getItemsNode(config) {
+        RED.nodes.createNode(this,config);
+        var bringCredentials = RED.nodes.getNode(config.bringCredentials);
+        var node = this;
+
+        node.on('input', async (msg) => {
+            try {          
+                msg.items = await bringCredentials.getItems(config.listUUID);
+                node.send(msg);
+            } catch (err) {
+                node.error(err);
+            }
+        });
+    }
+    RED.nodes.registerType('getItems',getItemsNode);
+
+
+    function loadTranslationsNode(config) {
+        RED.nodes.createNode(this,config);
+        var bringCredentials = RED.nodes.getNode(config.bringCredentials);
+        var node = this;
+
+        node.on('input', async (msg) => {
+            try {          
+                msg.translations = await bringCredentials.loadTranslations(config.locale);
+                node.send(msg);
+            } catch (err) {
+                node.error(err);
+            }
+        });
+    }
+    RED.nodes.registerType('loadTranslations',loadTranslationsNode);
+
+
+    function loadCatalogNode(config) {
+        RED.nodes.createNode(this,config);
+        var bringCredentials = RED.nodes.getNode(config.bringCredentials);
+        var node = this;
+
+        node.on('input', async (msg) => {
+            try {          
+                msg.catalog = await bringCredentials.loadCatalog(config.locale);
+                node.send(msg);
+            } catch (err) {
+                node.error(err);
+            }
+        });
+    }
+    RED.nodes.registerType('loadCatalog',loadCatalogNode);
+
+
+    function removeItemNode(config) {
+        RED.nodes.createNode(this,config);
+        var bringCredentials = RED.nodes.getNode(config.bringCredentials);
+        var node = this;
+
+        node.on('input', async (msg) => {
+            try {          
+                if (!msg.payload) {
+                    node.error("No payload given");
+                } else if (!msg.payload.listUUID) {
+                    node.error("No listUUID given");
+                } else if (!msg.payload.itemName) {
+                    node.error("No itemName given");
+                }
+                
+                setErrorOrResult(msg, await bringCredentials.removeItem(msg.payload.listUUID, msg.payload.itemName));
+
+                node.send(msg);
+            } catch (err) {
+                node.error(err);
+            }
+        });
+    }
+    RED.nodes.registerType('removeItem',removeItemNode);
+
+
+    function moveToRecentlyNode(config) {
+        RED.nodes.createNode(this,config);
+        var bringCredentials = RED.nodes.getNode(config.bringCredentials);
+        var node = this;
+
+        node.on('input', async (msg) => {
+            try {          
+                if (!msg.payload) {
+                    node.error("No payload given");
+                } else if (!msg.payload.listUUID) {
+                    node.error("No listUUID given");
+                } else if (!msg.payload.itemName) {
+                    node.error("No itemName given");
+                }
+                
+                setErrorOrResult(msg, await bringCredentials.moveToRecently(msg.payload.listUUID, msg.payload.itemName));
+
+                node.send(msg);
+            } catch (err) {
+                node.error(err);
+            }
+        });
+    }
+    RED.nodes.registerType('moveToRecently',moveToRecentlyNode);
+
+
+    function saveItemNode(config) {
+        RED.nodes.createNode(this,config);
+        var bringCredentials = RED.nodes.getNode(config.bringCredentials);
+        var node = this;
+
+        node.on('input', async (msg) => {
+            try {          
+                if (!msg.payload) {
+                    node.error("No payload given");
+                } else if (!msg.payload.listUUID) {
+                    node.error("No listUUID given");
+                } else if (!msg.payload.itemName) {
+                    node.error("No itemName given");
+                }
+
+                setErrorOrResult(msg, await bringCredentials.saveItem(msg.payload.listUUID, msg.payload.itemName, msg.payload.specification || ""));
+
+                node.send(msg);
+            } catch (err) {
+                node.error(err);
+            }
+        });
+    }
+    RED.nodes.registerType('saveItem',saveItemNode);
+
+
+    function getItemsDetailedNode(config) {
+        RED.nodes.createNode(this,config);
+        var bringCredentials = RED.nodes.getNode(config.bringCredentials);
+        var node = this;
+
+        node.on('input', async (msg) => {
+            try {          
+                msg.translations = await bringCredentials.loadTranslations(config.locale);
+                msg.items = await bringCredentials.getItems(config.listUUID);
+                msg.details = await bringCredentials.getItemsDetails(config.listUUID);
+                msg.catalog = await bringCredentials.loadCatalog(config.locale);
+
+                msg.itemsPurchase = [];
+                msg.items.purchase.forEach(item => {
+                    msg.itemsPurchase.push(toItemDetailed(item));
+                });
+                
+                msg.itemsRecently = [];
+                msg.items.recently.forEach(item => {
+                    msg.itemsRecently.push(toItemDetailed(item));
+                });
+
+                if (!msg.debug) {
+                    msg.translations = undefined;
+                    msg.items = undefined;
+                    msg.details = undefined;
+                    msg.catalog = undefined;
+                }
+
+                function toItemDetailed(item) {
+                    var itemDetailed = {
+                        name: item.name,
+                        displayName: msg.translations[item.name],
+                        specification: item.specification
+                    };
+                    itemDetailed.detailByName = msg.details.find(d => d.itemId === itemDetailed.name);
+                    itemDetailed.detailByDisplayName = itemDetailed.displayName ? msg.details.find(d => d.itemId === itemDetailed.displayName) : undefined;
+
+                    itemDetailed.iconUrl = 'https://web.getbring.com/assets/images/items/';
+                    if (itemDetailed.detailByName && itemDetailed.detailByName.userIconItemId != "") {
+                        itemDetailed.iconUrl += toUrlName(itemDetailed.detailByName.userIconItemId);
+                    } else if (itemDetailed.detailByDisplayName && itemDetailed.detailByDisplayName.userIconItemId != "") {
+                        itemDetailed.iconUrl += toUrlName(itemDetailed.detailByDisplayName.userIconItemId);
+                    } else {
+                        // CHECK CATALOG
+                        var found = false;
+                        msg.catalog.sections.forEach(section => {
+                            if (section.items.includes(itemDetailed.name)) {      
+                                itemDetailed.iconUrl += toUrlName(itemDetailed.name);
+                                found = true;
+                            }
+                        });
+
+                        if (!found) {
+                            itemDetailed.iconUrl += toUrlName(itemDetailed.name.substr(0,1));
+                        }
+                    }
+                    itemDetailed.iconUrl += '.png';
+
+                    if (!msg.debug) {
+                        itemDetailed.detailByName = undefined;
+                        itemDetailed.detailByDisplayName = undefined;
+                    }
+
+                    // set displayName to name if not set
+                    itemDetailed.displayName = itemDetailed.displayName ? itemDetailed.displayName : itemDetailed.name;
+
+                    return itemDetailed;
+                }
+
+                function toUrlName(name) {
+                    name = name.toLowerCase();
+                    name = name.replace('ä','ae');
+                    name = name.replace('ö','oe');
+                    name = name.replace('ü','ue');
+                    return name;
+                }
+
+                msg.payload = undefined;
+
+                node.send(msg);
+            } catch (err) {
+                node.error(err);
+            }
+        });
+    }
+    RED.nodes.registerType('getItemsDetailed',getItemsDetailedNode);
+};
+
+function setErrorOrResult(msg, result) {
+    if (result.length > 0) {
+        msg.error = result;
+    }
+}
